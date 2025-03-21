b2e03a0e0a5148a006977cd214caafcb2448e938
ercancelikbusiness <ercancelikbusiness@gmail.com>
2025-03-21 03:54:48 +0300

    .

--- src/functions.ts
+++ src/functions.ts
@@ -46,13 +46,14 @@ console.log(getAverage2(10,20,30,60));
 
 let getAverage3 = (...a:number[]):string => {  
     let toplam=0;
-let count=0; // aslında count' a gerek yok en son ortalama kısmında toplam/a.length yapabilirsin...
+//let count=0; // aslında count' a gerek yok en son ortalama kısmında toplam/a.length yapabilirsin...
 
 for(let i =0; i<a.length; i++){
     toplam=toplam+ a[i];
-    count++;
+   // count++;
 }
-const ortalama=toplam/count;
+//const ortalama=toplam/count;
+const ortalama = toplam/a.length;
 return 'ortalama : ' +ortalama;
 }
 
@@ -62,11 +63,144 @@ console.log(getAverage3(10,20));
 console.log(getAverage3(10,20,30));
 console.log(getAverage3(10,20,30,60));
 
+/*//--------------------------------------------------------------------------------------//
+//aşağıda javascript  fonksiyonu mevcuttur paremetrede tür belirtilmediği için  obje gönderilmesi için
+// örnek bir kod yazdık ama Ts de bu sorun olmaz çünkü paremetre zaten object olarak belirlenir.
+
+function greetUser(user) {
+    if (typeof user !== "object" || user === null) {
+        throw new Error("Fonksiyon sadece bir obje kabul eder!");
+    }
+    
+    console.log(`Merhaba, ${user.name}! Senin yaşın ${user.age} ve mesleğin ${user.job}.`);
+}
+
+greetUser({ name: "Ahmet", age: 30, job: "Mühendis" });
+*/
+
+//-----------------------aşağıda TypeScript formu mevcut Ts de türleri vb belirtmen gerek ---------------------
+
+function greetUser(user: { name: string, age: number, job: string }) {
+    console.log(`Merhaba, ${user.name}! Senin yaşın ${user.age} ve mesleğin ${user.job}.`);
+}
+
+greetUser({ name: "Ayşe", age: 28, job: "Doktor" });
+// greetUser("Ayşe"); // TypeScript derleme hatası verecektir
 
 
 
+//----------------------------? kullanımı ve diğer operatörler------------------------///
+
+function greetUser2(user: { name: string, age?: number, job?: string }) {
+
+    //console.log(`Merhaba, ${user.name}! ${user.age ? `Yaşın ${user.age}` : ""} ${user.job ? `ve mesleğin ${user.job}.` : ""}`);
+
+    //age ve jobopsiyonel olduğu için console.log içinde bunu sorgulamalıyız
+    //${user.job ? ` ${user.job} ` : "" => bu user job varsa user job u yazdır  eğer yoksa "" boşluk yazdır demektir
+    //yani ? varsa  : yoksa demek gibi düşün fakat ? dan sonra `` koymayı unutma 
+    //ama aşağıdaki format daha mantıklı
+
+    console.log("Merhaba " + user.name + (user.age ? " yaşın " + (user.age) : " yaşın yok") );
+    
 
 
+}
+
+greetUser2({ name: "Burak"}); // 
+greetUser2({name:"Ercan" , age:22});
+
+// operatör örnekleri devam ///
+
+let age = 20;
+console.log(age >= 18 ? 'Yetişkin' : 'Çocuk');
+// Çıktı: Yetişkin
+
+//yani ? varsa yada doğruysa gibi anlamlara gelir
+
+// --------------------?? operatörü ----------------------------//
+
+//let result = value ?? defaultValue;
+/*
+Eğer value null veya undefined ise, defaultValue döndürülür.
+Eğer value geçerli (yani null veya undefined değilse), o zaman value döndürülür.
+*/
+
+let userAge = undefined;
+console.log(userAge ?? 25);
+// Çıktı: 25
+
+let userAge2 = 20;
+console.log(userAge2 ?? 25);
+// Çıktı: 20
+
+// ----------------|| operatörü kullanım----------------//
+
+/*
+|| (Logical OR) Operatörü:
+Amaç: || operatörü, "truthy" ve "falsy" değerlere göre çalışır. Yani, falsy değerlere (örneğin false, 0, "", null, undefined, NaN) karşı sağlanan
+ alternatif değeri döndürür.
+Kullanım: a || b
+Eğer a değeri falsy bir değer (yani false, 0, "", null, undefined, NaN) ise, b döndürülür.
+Eğer a değeri truthy bir değer (yani geçerli bir değer) ise, a döndürülür.*/
 
 
+let userName = null;
+let defaultName = "Misafir";
 
+console.log(userName || defaultName);  // "Misafir"
+
+let userName2 = "";
+let defaultName2 = "Misafir";
+
+console.log(userName2 || defaultName2);  // "Misafir"
+
+let userName3 = "Burak";
+let defaultName3 = "Misafir";
+
+console.log(userName3 || defaultName3);  // "Burak"
+//Burada, userName geçerli bir değere (yani "Burak") sahip olduğu için || operatörü userName'i döndürür.
+
+
+// ------------------------&& örnekleri-----------------------------
+/*&& operatörü, iki değerin de "truthy" (geçerli, geçerli değer) olup olmadığını kontrol eder ve ilk "falsy" değeri bulana
+ kadar çalışmaya devam eder. Eğer her iki değer de truthy ise, sonuncusunu döndürür. Yani:
+
+Eğer iki değer de truthy ise, sonuncu değeri döndürür.
+Eğer bir değer falsy ise, ilk falsy değeri döndürür.*/
+
+console.log("------------------------------------------")
+
+let d = false;
+let c = "Misafir";
+let result = d && c;  
+console.log(result); // false
+
+let e = "burak";
+let f = "Misafir";
+let result2 = e && f;  
+console.log(result2); // Misafir
+
+console.log("---------------------------")
+let e1 = "asd";
+let e2 = "Misafir1";
+let e3 = "Misafir2";
+let e4 = false;
+let e5 = "Misafir4";
+
+let result3 = e1 && e2 &&e3&&e4&&e5;  
+console.log(result3);  //e4 deki  false oldugu için çıktı false'dir
+
+//yukarıdaki örnekte e4 de misafir 3 olsaydı o zamn çıktımız misafir 4 yani en sonuncu truthly değer olurdu..
+
+/*&& (AND) operatörü, tüm operandlar truthy olduğu sürece en sonuncusunu döndürür. Ancak, bir falsy değerle karşılaştığında, 
+ilk falsy değeri döndürür ve işlem sonlanır. false, null, undefined, 0, NaN, ve boş string ("") gibi değerler falsy olarak kabul edilir.
+*/
+
+
+// aşağıda result4 de j de işlem && ilşe durduğu için k ya falseyi atayamadık j true olsaydı k ya falseyi atardık.
+let j = false;
+let k = true;
+let result4 = j && (k = false);  // k = false olmaz, çünkü j false olduğunda işlem durmuştu
+console.log(k);  // true, çünkü b'nin değeri değişmedi
+
+// bu operatörlerde java ve javascript  benzer mantıktadır.
---
+++ src/interfaces.ts
@@ -0,0 +1,79 @@
+export{};
+
+// bir deüişkeni tür gibi gösteremeyiz bunun için  type  veya interface kullanmalıyız
+
+//örneğin aşağıda a değişkenini  deneme fonksiyonunun paremetresinde tür olarak kullanmak istedik hata verdi.
+
+/*
+let a={x:"ercan",y:25};
+
+let deneme = (degisken:a) =>{}
+*/
+
+// aşağıda type kullandık ve deneme1 fonk paremetresindeki değişkene tür olarak atayabildik
+//çünkü TS yukarda a yı değişken kabul ediyor aşağıda abc yi ise tür kabul ediyor bu TS ile alakalı
+
+type abc= {x:string,y:number};
+
+let deneme1 = (degisken:abc) =>{}
+
+//aslında a yı  typeof ile türleştirebiliriz örn:
+
+/*
+type AType = typeof a;
+*/ 
+// diyip AType ' ı paremetrede tür olarak kullanabiliriz a yı temsil eder...
+
+//------------------------------------------------------------------------------
+//---------------------yada interface kullanabiliriz ---------------------------
+//interface içinde sadece fonksiyonların (metod) imzaları olur gövdesini kullanmayız
+//genel bilgi: objeler , ile ayırabiliriz yada herbiri sonuna ; koyarız genelde ; kullanılır
+
+
+interface Point {
+    x: number,
+    y:number
+}
+
+interface Passenger{
+      name:string;
+      phone:string;
+}
+
+interface Vehicle {
+
+    currentLocation:Point;                                   // bu bir değişkendir türü Point
+    travelTo(point: Point):void;                            //bu dönüşsüz metod ( imzadan ibaret gövdesi yok)
+    getDistance(pointA: Point, pointB:Point):number;      // bu number döndüren bir metod ( imzadan ibaret gövdesi yok)
+    addPassenger(passenger:Passenger):void
+    removePassenger(passenger:Passenger):void;
+
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+let travelTo = (point:Point) =>{
+//..
+}
+
+let getDistance = (pointA:Point, pointB:Point)=>{
+    //...
+}
+
+travelTo({
+    x:2,
+    y:3
+});
+*/
+
